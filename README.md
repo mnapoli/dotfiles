41bef8d26c8d70e791fc05496e513f07cf79f3ad
Matthieu Napoli <matthieu@mnapoli.fr>
2014-04-21 17:10:16 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Usage
+
+Installation:
+
+```sh
+$ # install ZSH
+$ chsh -s `which zsh`
+$ git clone ...
+$ cd dotfiles
+$ ./install.sh
+```
+
+After installation, update composer dependencies:
+
+```sh
+$ composer global update
+```
+
+Update:
+
+```sh
+$ cd dotfiles
+$ ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Installs all dependencies needed
+# Will update if already installed
+
+set -e
+
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Oh my zsh
+if [ ! -d "$DOTFILES_DIR/.oh-my-zsh" ]; then
+    git clone git://github.com/robbyrussell/oh-my-zsh.git $DOTFILES_DIR/.oh-my-zsh
+else
+    (cd $DOTFILES_DIR/.oh-my-zsh; git pull)
+fi
+
+# Zsh syntax highlighting plugin
+if [ ! -d "$DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+else
+    (cd $DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting; git pull)
+fi
+
+# Zsh better autocompletion
+if [ ! -d "$DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-history-substring-search" ]; then
+    git clone git://github.com/zsh-users/zsh-history-substring-search.git $DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+else
+    (cd $DOTFILES_DIR/.oh-my-zsh/custom/plugins/zsh-history-substring-search; git pull)
+fi
+
+# Zsh suggestions while typing
+if [ ! -d "$DOTFILES_DIR/.zsh-autosuggestions" ]; then
+    git clone git://github.com/tarruda/zsh-autosuggestions.git $DOTFILES_DIR/.zsh-autosuggestions
+else
+    (cd $DOTFILES_DIR/.zsh-autosuggestions; git pull)
+fi
+
+# Configures git
+git config --global user.name "Matthieu Napoli"
+git config --global user.email matthieu@mnapoli.fr
+
+
+# Files & directories to symlink
+symlinks=".zshrc .oh-my-zsh .zsh-autosuggestions .gitignore_global .composer"
+
+# Create symlinks 
+for symlink in $symlinks; do
+    echo "Creating symlink for $symlink"
+    if [ -h ~/.$symlink ]; then
+        echo "Skipping $symlink: symlink already exist"
+    elif [ ! -e ~/.$symlink ]; then
+        ln -s $DOTFILES_DIR/$symlink ~/$symlink
+    else
+        echo "Error for $symlink: file already exist"
+    fi
+done
